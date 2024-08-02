535fa81d205819b913f60d4efc84759cc11bf927
Sunshine Hughes <sunshinehughes2011@gmail.com>
2024-08-02 16:36:57 -0400

    changing organization and order

--- src/App.js
+++ src/App.js
@@ -1,4 +1,32 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ClientAttendanceScreen from './components/ClientAttendanceScreen';
+import ClientDashboardScreen from './components/ClientDashboardScreen';
+// Import other components...
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/attendance" element={<ClientAttendanceScreen />} />
+        <Route path="/dashboard" element={<ClientDashboardScreen />} />
+        {/* Add other routes... */}
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
+
+
+
+
+
+
+
+
+
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
